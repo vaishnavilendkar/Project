2e1c5aa458ae5de13683cdcb712d9750a8534fec
vaishnavilendkar <124515047+vaishnavilendkar@users.noreply.github.com>
2023-02-13 13:58:46 +0530

    Add files via upload

---
+++ Customised Virtual File System.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Node
+{
+    public int Data;
+    public Node Next;
+}
+
+class SinglyLL
+{
+    public Node Head;
+    public int Count;
+
+    public SinglyLL()
+    {
+        Head = null;
+        Count = 0;
+    }
+}
+
+class Program445
+{
+    public static void main(String Arg[])
+    {
+        SinglyLL obj = new SinglyLL();
+    }
+}
